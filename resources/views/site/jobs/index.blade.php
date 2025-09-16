f729814e8a2ba51f07a4977d254c3e27f0205657
Jelson <jelsonlanto25@gmail.com>
2025-09-16 16:48:49 +0800

    Fix skill deletion, payment upload handling, and job search

--- resources/views/site/jobs/index.blade.php
+++ resources/views/site/jobs/index.blade.php
@@ -9,6 +9,27 @@
         </div>
         <div class="row">
             <div class="col-md-8 col-sm-12 mx-auto">
+                <form class="mb-4" method="GET" action="{{ url('jobs') }}">
+                    <div class="input-group">
+                        <input type="text" name="search" list="job-title-options" class="form-control"
+                            placeholder="Search for job title or location" value="{{ $search }}">
+                        <datalist id="job-title-options">
+                            @foreach ($jobTitles as $title)
+                                <option value="{{ $title }}"></option>
+                            @endforeach
+                        </datalist>
+                        <button class="btn btn-primary" type="submit">Search</button>
+                    </div>
+                    @if ($search)
+                        <div class="mt-2">
+                            <a href="{{ url('jobs') }}" class="btn btn-link p-0">Clear search</a>
+                        </div>
+                    @endif
+                </form>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-8 col-sm-12 mx-auto">
                 <!-- Search List -->
                 <ul class="searchList">
                     @forelse ($jobs as $job)
@@ -41,7 +62,13 @@
                         <div class="row">
                             <div class="col-md-8 col-sm-8 mx-auto">
                                 <div class="d-flex align-items-center" style="margin-top: 15px">
-                                    <h5>No jobs available, please set-up your skill sets to get a results.</h5>
+                                    <h5>
+                                        @if ($search)
+                                            No jobs found for "{{ $search }}".
+                                        @else
+                                            No jobs available, please set-up your skill sets to get results.
+                                        @endif
+                                    </h5>
                                 </div>
                             </div>
                         </div>
